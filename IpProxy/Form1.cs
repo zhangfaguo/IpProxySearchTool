21ff6e2f1608d96f82dbd0d3c1366a6e781d9411
zhangfaguo <zhangfaguo98@163.com>
2016-01-14 16:12:18 +0800

    change more

--- IpProxy/Form1.cs
+++ IpProxy/Form1.cs
@@ -67,7 +67,7 @@ namespace IpProxy
             this.FormClosed += Form1_FormClosed;
             closeTag = true;
 
-            for (var i = 0; i < 20; i++)
+            for (var i = 0; i < 40; i++)
             {
                 System.Threading.Tasks.Task.Factory.StartNew(() =>
                 {
